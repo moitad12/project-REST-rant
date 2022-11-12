73de7e96b79c006d36a3d3d575fc4191d16cf49e
Moises Tadeo <moisestadeo10@gmail.com>
2022-11-11 23:39:06 -0800

    Adding places view with mock data

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,26 @@
-const router = require('express').Router()
+const express = require('express')
+// const app = express()
+require('dotenv').config()
+const router = express.Router()
 
+// GET /places
 router.get('/', (req, res) => {
-    res.send('GET /places')
-})
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+    res.render('places/index', {places})
+  })
+
 
 module.exports = router
