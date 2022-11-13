d283cbdfc4e2805211df3615d4b041619e9896d6
Moises Tadeo <moisestadeo10@gmail.com>
2022-11-13 12:01:30 -0800

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,20 @@
 const express = require('express')
-require('dotenv').config()
 const router = express.Router()
 const places = require('../models/places.js')
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', {place: places[id]})
+  }
+})
+
 
 router.get('/new', (req, res) => {
   res.render('places/new')
